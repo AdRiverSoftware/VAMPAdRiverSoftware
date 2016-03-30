4e478b8ca5d5d074e5c4aaa1b8b734f39023ab05
AdRiverSoftware <adriversoftware@gmail.com>
2016-03-30 21:07:30 +0530

    Changes

--- www/admin/dashboard.php
+++ www/admin/dashboard.php
@@ -1,60 +1,167 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<title>AdRiver Adserver - Home</title>
+<meta name="generator" content="AdRiver Adserver v3.2.4 - http://www.revive-adserver.com">
+<meta name="robots" content="noindex, nofollow">
+<meta http-equiv="X-UA-Compatible" content="IE=7">
+<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="http://localhost/revive/www/admin/assets/images/favicon.ico" />
 
-/*
-+---------------------------------------------------------------------------+
-| Revive Adserver                                                           |
-| http://www.revive-adserver.com                                            |
-|                                                                           |
-| Copyright: See the COPYRIGHT.txt file.                                    |
-| License: GPLv2 or later, see the LICENSE.txt file.                        |
-+---------------------------------------------------------------------------+
-*/
+    <link rel="stylesheet" type="text/css" href="http://localhost/revive/www/admin/assets/min.php?g=oxp-css-ltr&amp;v=3.2.4">
 
-// Require the initialisation file
-require_once '../../init.php';
 
-// Required files
-require_once MAX_PATH . '/lib/OA/Dal.php';
-require_once MAX_PATH . '/lib/OA/Admin/Template.php';
-require_once MAX_PATH . '/www/admin/lib-statistics.inc.php';
-require_once MAX_PATH . '/lib/OA/Permission.php';
-require_once LIB_PATH . '/Admin/Redirect.php';
+<script type="text/javascript">
+<!--
+    var validatorPreferences = {
+        'strFieldContainsErrors': "The following fields contain errors:",
+        'strFieldFixBeforeContinue1': "Before you can continue you need",
+        'strFieldFixBeforeContinue2': "to correct these errors.",
+        'strWarningMissing': "Warning, possibly missing ",
+        'strWarningMissingOpening': " opening tag \'<\'",
+        'strWarningMissingClosing': " closing tag \'>\'",
+        'strSubmitAnyway': "Submit Anyway",
+        'thousandsSeperator': ","
+    };
 
-/*-------------------------------------------------------*/
-/* HTML framework                                        */
-/*-------------------------------------------------------*/
+	var tablePreferences = {
+		'warningBeforeDelete': true
+    };
+//-->
+</script>
+<script type="text/javascript" src="http://localhost/revive/www/admin/assets/min.php?g=oxp-js&amp;v=3.2.4"></script>
 
-require_once MAX_PATH . '/www/admin/config.php';
-OA_Permission::enforceAccount(OA_ACCOUNT_ADMIN, OA_ACCOUNT_MANAGER);
 
-// If the user is a manager and the dashboard can't be showed to him
-// clear the menu cache and redirect this user to advertiser-index.php
-if (OA_Permission::isAccount(OA_ACCOUNT_ADMIN) && !$GLOBALS['_MAX']['CONF']['ui']['dashboardEnabled']) {
-    OA_Admin_Menu::_clearCache(OA_ACCOUNT_ADMIN);
-    OX_Admin_Redirect::redirect('agency-index.php');
-}
-if (OA_Permission::isAccount(OA_ACCOUNT_MANAGER) && !$GLOBALS['_MAX']['CONF']['ui']['dashboardEnabled']) {
-    OA_Admin_Menu::_clearCache(OA_ACCOUNT_MANAGER);
-    OX_Admin_Redirect::redirect('advertiser-index.php');
-}
-if (OA_Permission::isAccount(OA_ACCOUNT_MANAGER) && !$GLOBALS['_MAX']['CONF']['sync']['checkForUpdates']) {
-    OA_Admin_Menu::_clearCache(OA_ACCOUNT_MANAGER);
-    OX_Admin_Redirect::redirect('advertiser-index.php');
-}
 
-$widget = !empty($_REQUEST['widget']) ? $_REQUEST['widget'] : 'Index';
 
-if (preg_match('/^[a-z0-9]+$/i', $widget) && file_exists(MAX_PATH.'/lib/OA/Dashboard/Widgets/'.$widget.'.php')) {
-    // Load widget
-    require(MAX_PATH.'/lib/OA/Dashboard/Widgets/'.$widget.'.php');
-    $widget = 'OA_Dashboard_Widget_'.$widget;
-} else {
-    // Show empty widget
-    require(MAX_PATH.'/lib/OA/Dashboard/Widget.php');
-    $widget = 'OA_Dashboard_Widget';
-}
 
-$oDashboard = new $widget($_REQUEST);
-$oDashboard->display();
+<script type='text/javascript' src='http://localhost/revive/www/delivery/fl.js'></script>
 
-?>
+
+</head>
+<body class="hasInterface   " onload="initPage();">
+
+
+
+
+<div id="oaHeader">
+
+    <div id="oaHeaderBranding" class="brandingAdServer">AdRiver Adserver</div>
+  <div id="oaNavigationExtraTop" class="hasSearch">
+    <ul>
+
+        <li class="infoUser">admin</li>
+
+        <li class="buttonHelp"><a href="http://documentation.revive-adserver.com/user/dashboard" target="help">Help</a></li>
+
+        <li class="buttonSupport"><a href="http://www.revive-adserver.com/support/" target="_blank">Support</a></li>
+
+        <li class="buttonLogout"><a href="http://localhost/revive/www/admin/logout.php">Logout</a></li>
+
+    </ul>
+    </div>
+
+
+    <div id="oaSearch">
+        <form name="search" action="http://localhost/revive/www/admin/admin-search.php" target="SearchWindow" onsubmit="search_window(document.search.keyword.value,'http://localhost/revive/www/admin/admin-search.php'); return false;">
+            <input type="text" name="keyword" class="search" accesskey="<u>S</u>earch"><input class="submit" type="submit" value="&nbsp" >
+        </form>
+    </div>
+</div>
+
+<div id="oaNavigation">
+    <ul id="oaNavigationTabs">
+      <li class="active  first">
+        <div class="left"><div class="right">
+          <a href="http://localhost/revive/www/admin/dashboard.php" accesskey="Home">Home</a>
+        </div></div>
+      </li>
+      <li class="passive  after-active">
+        <div class="left"><div class="right">
+          <a href="http://localhost/revive/www/admin/stats.php" accesskey="Home">Statistics</a>
+        </div></div>
+      </li>
+      <li class="passive ">
+        <div class="left"><div class="right">
+          <a href="http://localhost/revive/www/admin/advertiser-index.php" accesskey="Home">Inventory</a>
+        </div></div>
+      </li>
+      <li class="passive  last">
+        <div class="left"><div class="right">
+          <a href="http://localhost/revive/www/admin/account-index.php" accesskey="Home">Preferences</a>
+        </div></div>
+      </li>
+    </ul>
+
+    <ul id="oaNavigationExtra">
+        <li class="accountSwitcher">
+            <div class="triggerContainer"><div class="switchTrigger">Default manager</div> <a href="#" accesskey="w"><u>W</u>orking as</a> </div>
+            <div class="accountsPanel">
+              <div class="switchTop">&nbsp;</div>
+              <div class="switchMiddle">
+                <div class="switchMiddleBody">
+	                <div class="label">
+                      Switch to
+                                          </div>
+                    <div id="accountLoading"></div>
+	                <div class="result">
+                      
+<div>
+<ul id="accounts">
+  <li style="display: none"></li>
+      <li>
+      <a href="http://localhost/revive/www/admin/account-switch.php?account_id=1">Administrator account</a>
+    </li>
+    
+      <li class="opt">Manager for...</li>
+          <li class="inopt">
+        <a href="http://localhost/revive/www/admin/account-switch.php?account_id=2">Default manager</a>
+      </li>      
+            
+  </ul>
+</div>	                </div>
+                </div>
+              </div>
+              <div class="switchDown">&nbsp;</div>
+            </div>
+        </li>
+    </ul>
+    <script type='text/javascript'>
+    
+      <!--
+      $(document).ready(function() {
+    
+        initAccoutSwitcher("http://localhost/revive/www/admin/account-switch-search.php");
+    
+      });
+      -->
+    
+    </script>
+    <div class='accountSwitcherOverlay'>&nbsp;</div>
+</div>
+
+<div id="firstLevelContent">
+
+
+<div id="secondLevelContent">
+  
+<pre>
+<h1>
+
+Welcome to AdRiver Adserver...
+
+</h1>
+
+</pre>
+
+
+<!iframe src="http://localhost/revive/www/admin/dashboard.php?widget=Grid" width="100%" height="850" frameborder="0" marginwidth="0" marginheight="0"><!/iframe>
+
+
+
+      </div>
+</div>
+
+
+
+
+</body>
+</html>
